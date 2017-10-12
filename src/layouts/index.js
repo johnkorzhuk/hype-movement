c2bff4600303e08898fc2294725d9c844c747a3a
John Korzhuk <jkorzhuk@gmail.com>
2017-10-12 14:17:00 -0400

    removed fav folder from static

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -7,9 +7,12 @@ import Container from '../components/Container';
 import InstagramLink from '../components/links/Instagram';
 import BuyNow from '../components/links/BuyNow';
 import HypeLogo from '../components/logos/Hype';
-import Hero from '../components/Hero';
 
 import bgImage from '../assets/img/bg.jpg';
+import appleTouch from '../assets/fav/apple-touch-icon.png';
+import fav32 from '../assets/fav/favicon-32x32.png';
+import fav16 from '../assets/fav/favicon-16x16.png';
+import manifest from '../assets/fav/manifest.json';
 
 import './index.css';
 
@@ -81,24 +84,10 @@ const TemplateWrapper = ({ children }) => (
         { name: 'description', content: 'The Hype Movement' },
         { name: 'keywords', content: 'The Hype Movement, Hype Movement' }
       ]}>
-      <link
-        rel="apple-touch-icon"
-        sizes="180x180"
-        href={`${__PATH_PREFIX__}/apple-touch-icon.png`}
-      />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="32x32"
-        href={`${__PATH_PREFIX__}/favicon-32x32.png`}
-      />
-      <link
-        rel="icon"
-        type="image/png"
-        sizes="16x16"
-        href={`${__PATH_PREFIX__}/favicon-16x16.png`}
-      />
-      <link rel="manifest" href={`${__PATH_PREFIX__}/manifest.json`} />
+      <link rel="apple-touch-icon" sizes="180x180" href={appleTouch} />
+      <link rel="icon" type="image/png" sizes="32x32" href={fav32} />
+      <link rel="icon" type="image/png" sizes="16x16" href={fav16} />
+      <link rel="manifest" href={manifest} />
       <meta name="theme-color" content="#ffffff" />
     </Helmet>
     {children()}
